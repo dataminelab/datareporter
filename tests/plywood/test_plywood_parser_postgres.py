45a43fe4bbf8a0d5b604e1263e46c3ab0927e5ed
Romucmad <romanhri@mail.ru>
2021-07-02 12:00:28 +0300

    tests for parsers

---
+++ tests/plywood/test_plywood_parser_postgres.py
@@ -0,0 +1,65 @@
+import unittest
+
+from redash.plywood.query_parser import PlywoodQueryParser
+from tests.plywood.fixtures.test_data_0_split_1_measure_1_filter.sample_1 import POSTGRES_0_SPLIT_JOBS, \
+    POSTGRES_0_SPLIT_SHAPE, POSTGRES_0_SPLIT_RESULT
+from tests.plywood.fixtures.test_data_0_split_1_measure_1_filter_timeshift.sample_1 import \
+    POSTGRES_0_SPLIT_JOBS_TIMESHIFT, POSTGRES_0_SPLIT_SHAPE_TIMESHIFT, POSTGRES_0_SPLIT_RESULT_TIMESHIFT
+from tests.plywood.fixtures.test_data_1_split_1_measure_1filter_timeshift.sample_1 import \
+    POSTGRES_1_SPLIT_JOBS_TIMESHIFT, POSTGRES_1_SPLIT_SHAPE_TIMESHIFT, POSTGRES_1_SPLIT_RESULT_TIMESHIFT
+from tests.plywood.fixtures.test_data_1_split_1_mesture_1_filter.sample_1 import POSTGRES_1_SLIT_JOBS, \
+    TEST_DATA_1_SPLIT_SHAPE, POSTGRES_1_SPLIT_RESULT
+
+CUSTOMER_DATA_CUBE = 'customer'
+ENGINE = 'postgres'
+
+
+class TestPostgresParse(unittest.TestCase):
+
+    def test_0_split_1_measure_1_filter(self):
+        parser = PlywoodQueryParser(
+            data_cube_name=CUSTOMER_DATA_CUBE,
+            query_result=POSTGRES_0_SPLIT_JOBS,
+            shape=POSTGRES_0_SPLIT_SHAPE,
+
+        )
+
+        data = parser.parse_ply(ENGINE)
+
+        self.assertDictEqual(data, POSTGRES_0_SPLIT_RESULT)
+
+    def test_1_split_1_measure_1_filter(self):
+        parser = PlywoodQueryParser(
+            data_cube_name=CUSTOMER_DATA_CUBE,
+            query_result=POSTGRES_1_SLIT_JOBS,
+            shape=TEST_DATA_1_SPLIT_SHAPE,
+
+        )
+
+        data = parser.parse_ply(ENGINE)
+
+        self.assertDictEqual(data, POSTGRES_1_SPLIT_RESULT)
+
+    def test_0_split_1_measure_1_filter_time_shift(self):
+        parser = PlywoodQueryParser(
+            data_cube_name=CUSTOMER_DATA_CUBE,
+            query_result=POSTGRES_0_SPLIT_JOBS_TIMESHIFT,
+            shape=POSTGRES_0_SPLIT_SHAPE_TIMESHIFT,
+
+        )
+
+        data = parser.parse_ply(ENGINE)
+
+        self.assertDictEqual(data, POSTGRES_0_SPLIT_RESULT_TIMESHIFT)
+
+    def test_1_split_1_measure_1filter_time_shift(self):
+        parser = PlywoodQueryParser(
+            data_cube_name=CUSTOMER_DATA_CUBE,
+            query_result=POSTGRES_1_SPLIT_JOBS_TIMESHIFT,
+            shape=POSTGRES_1_SPLIT_SHAPE_TIMESHIFT,
+
+        )
+
+        data = parser.parse_ply(ENGINE)
+
+        self.assertDictEqual(data, POSTGRES_1_SPLIT_RESULT_TIMESHIFT)

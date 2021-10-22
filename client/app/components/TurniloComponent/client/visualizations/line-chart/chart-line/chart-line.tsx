1cd803b564fb5283a9f9ca798e2ca3240af19357
Romucmad <romanhri@mail.ru>
2021-10-22 16:41:49 +0200

    fixed client and tests added

--- client/app/components/TurniloComponent/client/visualizations/line-chart/chart-line/chart-line.tsx
+++ client/app/components/TurniloComponent/client/visualizations/line-chart/chart-line/chart-line.tsx
@@ -22,7 +22,6 @@ import { Unary } from "../../../../common/utils/functional/functional";
 import { ContinuousRange, ContinuousScale } from "../utils/continuous-types";
 import "./chart-line.scss";
 import { prepareDataPoints } from "./prepare-data-points";
-import {element} from "prop-types";
 
 export type Scale = d3.scale.Linear<number, number>;
 
@@ -53,14 +52,10 @@ export const ChartLine: React.SFC<ChartLineProps> = props => {
   const scaledPoints = points.map(([x, y]) => [xScale(x), yScale(y)] as [number, number]);
   const hasMultiplePoints = points.length > 1;
   const hasSinglePoint = points.length === 1;
-
-
   return <g className="chart-line" transform={stage.getTransform()}>
-    {hasMultiplePoints && <path ref={(node)=>{
-        if(node){
-            node.style.setProperty('fill','transparent','important')
-        }
-    }} className="line" d={line(scaledPoints)}  style={...stroke(color, dashed)} />}
+    {hasMultiplePoints && <path ref={node => {
+        if (node) node.style.setProperty("fill", "transparent", "important");
+    }} className="line" d={line(scaledPoints)}  style={stroke(color, dashed)} />}
     {hasMultiplePoints && showArea && <path className="area" d={area(scaledPoints)} />}
     {hasSinglePoint && <circle
       className="singleton"

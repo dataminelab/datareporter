e75e5b43893980a63a83c846962a38661192e139
mirkan1 <mirkanbaba1@gmail.com>
2023-02-26 21:19:37 -0500

    easier server reset for debugging implemented

--- bin/start_server_debug.sh
+++ bin/start_server_debug.sh
@@ -1,2 +1,3 @@
-rm  ~/.docker -r
-docker-compose up -d && docker-compose stop server && docker-compose run --rm --service-ports server debug && docker-compose start server
+export skip_dev_deps="do not update dev dependencies for debugging"
+docker-compose up -d
+docker-compose stop server && docker-compose run --rm --service-ports server debug && docker-compose start server

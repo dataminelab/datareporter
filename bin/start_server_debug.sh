7f7886e4201c19a0638503e0e67bd421cdae25ed
mirkan1 <mirkanbaba1@gmail.com>
2023-04-17 02:49:34 -0400

    skipping more dependencies for debugging start

--- bin/start_server_debug.sh
+++ bin/start_server_debug.sh
@@ -1,3 +1,4 @@
-export skip_dev_deps="do not update dev dependencies for debugging"
+export skip_dev_deps="do not update dev dependencies"
+export skip_ds_deps="do not update dev dependencies"
 docker-compose up -d
 docker-compose stop server && docker-compose run --rm --service-ports server debug && docker-compose start server

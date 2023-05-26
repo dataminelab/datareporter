99ec99a2bb02f0da1517ca3d0df3e30621744e2e
mirkan1 <mirkanbaba1@gmail.com>
2023-05-26 18:55:58 -0400

    unused function call cleaned

--- client/app/services/report.js
+++ client/app/services/report.js
@@ -43,13 +43,6 @@ function collectParams(parts) {
   return parameters;
 }
 
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
 export class Report {
   constructor(report) {
     extend(this, report);

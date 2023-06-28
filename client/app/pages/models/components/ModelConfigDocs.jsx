f39347c9a0b5fbf2a1cded2ac2102083b2084c3e
rathko <radek@dataminelab.com>
2023-06-28 22:48:50 +0300

    Updated docs

--- client/app/pages/models/components/ModelConfigDocs.jsx
+++ client/app/pages/models/components/ModelConfigDocs.jsx
@@ -7,51 +7,12 @@ function ModelConfigDocs() {
       <h1>
         <strong>Model&nbsp;</strong>
       </h1>
-      <p>
     <span style={{ fontWeight: 400 }}>
-      The model stores information about the data source. The model stores table
-      information from a data source. The model is the setting of the model to
-      display data from the database.
+    Start your journey by embarking on the creation of your very own model, an essential feature that captures critical details from your data source. Think of this model as your roadmap, guiding you to the data you need from your database.
+    <p></p>
     </span>
-      </p>
-      <p>
-    <span style={{ fontWeight: 400 }}>
-      To create a model, you need to click on the New Model button
-    </span>
-      </p>
-      <p>
-    <span style={{ fontWeight: 400 }}>
-      The model creation form will be displayed.
-    </span>
-      </p>
-      <p>
-        <span style={{ fontWeight: 400 }}>With fields</span>
-      </p>
-      <p>
-        <strong>name</strong>
-        <span style={{ fontWeight: 400 }}>: model name</span>
-      </p>
-      <p>
-        <strong>connection</strong>
-        <span style={{ fontWeight: 400 }}>: database connection</span>
-      </p>
-      <p>
-        <strong>table</strong>
-        <span style={{ fontWeight: 400 }}>: table from database</span>
-      </p>
-      <p>
-    <span style={{ fontWeight: 400 }}>
-      After creating the model you redirect to the config editor page of the
-      model.
-    </span>
-      </p>
-      <p>
-        <a href="https://youtu.be/K7qsB0Gx95g" target="_blank">
-          <span style={{ fontWeight: 400 }}>https://youtu.be/K7qsB0Gx95g</span>
-        </a>
-      </p>
       <h1>
-        <strong>Configuring Model</strong>
+        <strong>Configuring Your Model</strong>
       </h1>
       <h2>
         <strong>Overview</strong>
@@ -67,7 +28,7 @@ function ModelConfigDocs() {
     </span>
       </p>
       <h2>
-        <strong>Basic data cube properties</strong>
+        <strong>Let's dive into some basic properties you can adjust:</strong>
       </h2>
       <p>
     <span style={{ fontWeight: 400 }}>
@@ -76,211 +37,147 @@ function ModelConfigDocs() {
       </p>
       <p>
         <strong>name</strong>
-        <strong> (string)</strong>
       </p>
       <p>
-        <strong>
-          The name of the data cube as used internally in Data reporter and used in
-          the URLs. This should be a URL safe string. Changing this property for a
-          given data cube will break any URLs that someone might have generated for
-          that data cube in the past.
-        </strong>
+      <span style={{ fontWeight: 400 }}>
+        How Data reporter recognizes your data cube. It's URL safe, but remember, any changes to this will disrupt any previously generated URLs tied to your data cube.
+        </span>
       </p>
       <p>
         <strong>title</strong>
-        <strong> (string)</strong>
       </p>
       <p>
         <strong>
-          The user visible name that will be used to describe this data cube in the
-          UI. It is always safe to change this.
+        This will be the public-facing name for your data cube.
         </strong>
       </p>
       <p>
-        <strong>&nbsp;timeAttribute: sometimeLater</strong>
+        <strong>&nbsp;timeAttribute</strong>
+      </p>
+      <p>
+      <span style={{ fontWeight: 400 }}>
+        A selected time dimension, for example, 'created_date'.
+        </span>
       </p>
       <p>
         <strong>clusterName</strong>
-        <strong> (string)</strong>
       </p>
       <p>
-        <strong>
-          The cluster that the data cube belongs to (or “native” if this is a file
-          based data cube).
-        </strong>
+      <span style={{ fontWeight: 400 }}>
+        Denotes which cluster your data cube belongs to (default: native)
+        </span>
       </p>
       <p>
         <strong>defaultSortMeasure</strong>
-        <strong> (string), default: the first measure</strong>
+        <strong>default: the first measure</strong>
       </p>
       <p>
-        <strong>
-          The name of the measure that will be used for default sorting. It is
-          commonly set to the measure that represents the count of events.
-        </strong>
+      <span style={{ fontWeight: 400 }}>
+        The name of the measure that will be used for default sorting.
+        </span>
       </p>
       <p>
         <strong>defaultSelectedMeasures</strong>
-        <strong> (string[]), default: first four measures</strong>
+        <strong> default: first four measures</strong>
       </p>
       <p>
-        <strong>The names of the measures that will be selected by default.</strong>
+      <span style={{ fontWeight: 400 }}>
+          The names of the measures that will be selected by default.
+          </span>
       </p>
       <h2>
         <strong>Attribute Overrides</strong>
       </h2>
       <p>
         <strong>
-          While a Data reporter tries to learn as much as it can from your data cube
-          from a data source. It can not (yet) do a perfect job. The{" "}
+        There might be instances where additional guidance is required for the Data reporter. The attributeOverrides: section allows for such intervention, where you can input the name of the attribute (column) from the data source.
         </strong>
-        <strong>attributeOverrides:</strong>
-        <strong> section of the data cube is there for you to fix that.</strong>
       </p>
       <p>
         <strong>name</strong>
-        <strong> (string)</strong>
       </p>
       <p>
-        <strong>The name of the attribute (column) from data source.&nbsp;</strong>
+      <span style={{ fontWeight: 400 }}>
+          The name of the attribute (column) from data source.&nbsp;
+      </span>
       </p>
       <p>
-        <strong>
+      <span style={{ fontWeight: 400 }}>
           Here are some common scenarios where you should add an attribute override:
-        </strong>
+        </span>
       </p>
       <h2>
         <strong>Dimensions</strong>
       </h2>
       <p>
         <strong>
-          In this section you can define the dimensions that users can{" "}
-        </strong>
-        <strong>
-          <em>split</em>
-        </strong>
-        <strong> and </strong>
-        <strong>
-          <em>filter</em>
-        </strong>
-        <strong>
-          {" "}
-          on in the UI. Dimensions may be organized as list or tree where each item
-          of list can be either a dimension or{" "}
-        </strong>
-        <a href="https://allegro.github.io/turnilo/configuration-datacubes.html#dimension-group">
-          <strong>dimension group</strong>
-        </a>
-        <strong>
-          {" "}
-          having its own dimensions list or tree. The order of the dimension list in
-          the top of the left panel is determined by the order of the dimensions
-          definitions in this section.
+        In the realm of data, dimensions are crucial as they add context to our numbers. Here's what you need to know about defining them:
         </strong>
       </p>
       <h3>
         <strong>Dimension</strong>
       </h3>
       <p>
-        <strong>Dimensions are defined with following attributes:</strong>
+      <span style={{ fontWeight: 400 }}>
+          In the realm of data, dimensions are crucial as they add context to our numbers. Here's what you need to know about defining them. Dimensions are defined with following attributes:</span>
       </p>
       <p>
         <strong>name</strong>
-        <strong> (string)</strong>
       </p>
       <p>
-        <strong>
-          The name of the dimension. This does not have to correspond to the
-          attribute name (but the auto generated dimensions do). This should be a
-          URL safe string and unique across dimensions, dimension groups, measures
-          and measure groups. Changing this property will break any URLs that
-          someone might have generated that include this dimension.
-        </strong>
+      <span style={{ fontWeight: 400 }}>
+        Every dimension has a unique identifier that's URL safe. This name doesn't have to match the attribute name, but it does need to be unique across all dimensions, dimension groups, measures, and measure groups. Do bear in mind that if you alter this name, any pre-existing reports featuring this dimension may be disrupted.
+        </span>
       </p>
       <p>
         <strong>title</strong>
-        <strong> (string)</strong>
       </p>
       <p>
-        <strong>
-          The title for this dimension in the UI. Can be anything and is safe to
-          change at any time.
-        </strong>
+      <span style={{ fontWeight: 400 }}>
+        This refers to the displayed name for the dimension in the UI. Feel free to set this to anything you like; it's completely safe to alter at any time and won't impact any underlying functions or pre-existing reports.
+        </span>
       </p>
       <p>&nbsp;</p>
       <h2>
         <strong>Measures</strong>
       </h2>
       <p>
-        <strong>In this section you can define the measures that users can </strong>
-        <strong>
-          <em>aggregate</em>
-        </strong>
-        <strong> on (</strong>
-        <strong>
-          <em>apply</em>
-        </strong>
-        <strong>
-          ) on in the UI. Measures may be organized as list or tree where each item
-          of list can be either a measure or{" "}
-        </strong>
-        <a href="https://allegro.github.io/turnilo/configuration-datacubes.html#measure-group">
-          <strong>measure group</strong>
-        </a>
-        <strong>
-          {" "}
-          having its own measures list or tree. The order of the measure list in the
-          bottom of the left panel is determined by the order of the measure
-          definitions in this section.
-        </strong>
+      <span style={{ fontWeight: 400 }}>
+          Measures, on the other hand, represent the actual quantities or counts in our data. When defining your measures, here are the elements you need to consider:
+        </span>
       </p>
       <h3>
         <strong>Measure</strong>
       </h3>
       <p>
-        <strong>Measures are defined with following attributes:</strong>
+      <span style={{ fontWeight: 400 }}>
+          Measures are defined with following attributes:
+        </span>
       </p>
       <p>
         <strong>name</strong>
-        <strong> (string)</strong>
       </p>
       <p>
-        <strong>
-          The name of the measure. This should be a URL safe string. Changing this
-          property will break any URLs that someone might have generated that
-          include this measure.
-        </strong>
+      <span style={{ fontWeight: 400 }}>
+        Each measure requires a unique, URL-safe name. Similar to dimensions, changing this identifier can potentially disrupt any URLs that previously included this measure.
+        </span>
       </p>
       <p>
         <strong>title</strong>
-        <strong> (string)</strong>
+      </p>
+      <p>
+      <span style={{ fontWeight: 400 }}>
+        This is the displayed name for the measure in the UI. It can be modified at any time without any repercussions to your data or reports.
+        </span>
       </p>
       <p>
         <strong>formula</strong>
-        <strong> (string - plywood expression)</strong>
+        <strong> (string: plywood expression)</strong>
       </p>
       <p>
-        <strong>The </strong>
-        <a href="http://plywood.imply.io/expressions">
-          <strong>Plywood expression</strong>
-        </a>
-        <strong> for this dimension. By default it is </strong>
-        <strong>$main.sum($name)</strong>
-        <strong> where </strong>
-        <strong>
-          <em>name</em>
-        </strong>
-        <strong> is the name of the measure.</strong>
-      </p>
-      <p>
-        <strong>The </strong>
-        <strong>$main</strong>
-        <strong>
-          {" "}
-          part of the measure expressions serves as a placeholder for the data
-          segment. In Plywood every aggregate is a function that acts on a data
-          segment.
-        </strong>
+      <span style={{ fontWeight: 400 }}>
+          This is the specific Plywood expression associated with your measure. By default, it's set to $main.sum($name), where 'name' stands for the measure's name. In this formula, $main acts as a placeholder for your data segment, keeping in line with Plywood's principle that every aggregate is a function acting on a data segment.
+        </span>
       </p>
     </>
   )

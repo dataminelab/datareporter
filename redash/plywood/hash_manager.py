6e65f790c489c504eb4703d36901b4baf30dcba7
mirkan1 <mirkanbaba1@gmail.com>
2023-07-06 11:58:39 -0400

    prev commit's missing addition

--- redash/plywood/hash_manager.py
+++ redash/plywood/hash_manager.py
@@ -176,14 +176,16 @@ def parse_result(
     # IndexError: list index out of range
     if errored:
         data = None
+        meta = None
     else:
         data = query_parser.parse_ply(data_cube.ply_engine)
+        meta = data_cube.get_meta(queries)
 
     serializer = ReportSerializer(
         queries=queries,
         failed=errored,
         data=data,
-        meta=data_cube.get_meta(queries),
+        meta=meta,
         shape=expression.shape,
     )
 

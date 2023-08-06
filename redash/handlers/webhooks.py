fde8249485b838e644c7d78eb58783d9afc5eb2f
jaro <jaroslaw.herod@gmail.com>
2023-08-06 11:47:29 +0200

    chore: add lock to allow only single worker execution

--- redash/handlers/webhooks.py
+++ redash/handlers/webhooks.py
@@ -1,24 +1,42 @@
-import subprocess
-import json
 import base64
+import json
 import logging
+import subprocess
+import threading
+
 from flask import request
 
 from redash.handlers import routes
-from redash.handlers.base import json_response
 
 logger = logging.getLogger(__name__)
+
+locks = {}
+
+
+class WorkerProcess:
+    def __int__(self, queue: str):
+        self._queue = queue
+
+    def execute(self):
+        command = f"/app/manage.py rq worker --burst {self._queue}"
+        queue_lock = locks[self._queue]
+        if queue_lock is None:
+            queue_lock = locks[self._queue] = threading.Semaphore()
+        if queue_lock.acquire(blocking=False):
+            logger.info(f"running - {command}")
+            result = subprocess.run(command, shell=True, capture_output=True)
+            queue_lock.release()
+            logger.warning(
+                f"worker execution result : {result.returncode}\nstdout:\n {result.stdout}\nstderr:\n {result.stderr}")
+        else:
+            logger.info("other worker is already running - skipping execution")
 
 
 @routes.route("/api/subscribe/default", methods=["POST"])
 def pubsub_subscribe():
     queue = get_message_from_subscribed_data(request.data)
-    command = f"/app/manage.py rq worker --burst {queue}"
-
-    logger.info(f"running - {command}")
-    result = subprocess.run(command, shell=True)
-    logger.warning(f"worker execution result : {result.returncode}\nstdout:\n {result.stdout}\nstderr:\n {result.stderr}")
-
+    worker = WorkerProcess(queue)
+    worker.execute()
     return ("", 204)
 
 

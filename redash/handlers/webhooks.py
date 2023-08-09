099d6bd121125451d973d45b58481938189b3932
jaro <jaroslaw.herod@gmail.com>
2023-08-09 20:53:31 +0200

    chore: fix blocking of new worker startup

--- redash/handlers/webhooks.py
+++ redash/handlers/webhooks.py
@@ -14,22 +14,29 @@ locks = {}
 
 
 class WorkerProcess:
-    def __int__(self, queue: str):
-        self._queue = queue
+    queue = None
+    lock = None
 
-    def execute(self):
-        command = f"/app/manage.py rq worker --burst {self._queue}"
-        queue_lock = locks[self._queue]
-        if queue_lock is None:
-            queue_lock = locks[self._queue] = threading.Semaphore()
-        if queue_lock.acquire(blocking=False):
-            logger.info(f"running - {command}")
-            result = subprocess.run(command, shell=True, capture_output=True)
-            queue_lock.release()
-            logger.warning(
-                f"worker execution result : {result.returncode}\nstdout:\n {result.stdout}\nstderr:\n {result.stderr}")
+    def __init__(self, queue: str):
+        self.queue = queue
+        if self.queue in locks:
+            self.lock = locks[self.queue]
+        else:
+            self.lock = locks[self.queue] = threading.Semaphore()
+
+    def execute(self) -> None:
+        if self.lock.acquire(blocking=False):
+            self.run_worker()
+            self.lock.release()
         else:
             logger.info("other worker is already running - skipping execution")
+
+    def run_worker(self) -> None:
+        command = f"/app/manage.py rq worker --burst {self.queue}"
+        logger.info(f"running - {command}")
+        result = subprocess.run(command, shell=True, capture_output=True)
+        logger.warning(
+            f"worker execution result : {result.returncode}\nstdout:\n {result.stdout}\nstderr:\n {result.stderr}")
 
 
 @routes.route("/api/subscribe/default", methods=["POST"])
@@ -40,8 +47,9 @@ def pubsub_subscribe():
     return ("", 204)
 
 
-def get_message_from_subscribed_data(request_data):
+def get_message_from_subscribed_data(request_data: bytes) -> str:
     envelope = request_data.decode("utf-8")
     data = json.loads(envelope)
+    logger.info(f"Received message {envelope}")
     message_data = data['message']['data']
     return base64.b64decode(message_data).decode("utf-8").strip()

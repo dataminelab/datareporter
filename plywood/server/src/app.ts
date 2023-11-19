02f550dec913f5e9b212641e5144bcf88f54a727
jaro <jaroslaw.herod@gmail.com>
2023-11-19 10:18:53 +0100

    DR-179: enable branch and commit for sentry

--- plywood/server/src/app.ts
+++ plywood/server/src/app.ts
@@ -25,6 +25,7 @@ if (env.SENTRY_DSN != undefined) {
     console.log("Sentry enabled");
     Sentry.init({
         dsn: env.SENTRY_DSN,
+        release: env.DATAREPORTER_VERSION || 'unknown',
         environment: env.SENTRY_ENVIRONMENT,
         attachStacktrace: true,
         sampleRate: parseFloat(env.SENTRY_SAMPLE_RATE || "100.0",),

4675a93a0ff40cb9dc3bde02496c5095cc9f4d17
renas <mirkanbaba1@gmail.com>
2023-12-01 22:50:38 -0500

    Revert "FAILED_QUERY_CODE exact match test"
    This reverts commit 60e5319c6cb1175de4be29e6069efa27e43aeacb.

--- redash/plywood/hash_manager.py
+++ redash/plywood/hash_manager.py
@@ -214,7 +214,7 @@ def clean_errored(queries: list):
     errored = []
 
     for index, query in enumerate(queries):
-        if 'job' in query and query['job']['status'] == FAILED_QUERY_CODE:# and query['job']['error']:
+        if 'job' in query:
             errored.append(query)
             del queries[index]
 

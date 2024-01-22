358e081327b2df4a6fa7f3ecceff55d01e70a649
mirkan-samsung-wsl <mirkanbaba1@gmail.com>
2024-01-22 21:27:01 +0300

    Update restart_server.sh script

--- bin/restart_server.sh
+++ bin/restart_server.sh
@@ -1,7 +1,8 @@
 docker stop datareporter-server-1
 docker rm datareporter-server-1
 docker rmi datareporter-server
-export skip_dev_deps="do not update dev dependencies"
-export skip_ds_deps="do not update ds dependencies"
+#export skip_dev_deps="do not update dev dependencies"
+#export skip_ds_deps="do not update ds dependencies"
+#export skip_frontend_build="do not build front-end"
 docker-compose up -d
 docker-compose stop server && docker-compose run --rm --service-ports server debug && docker-compose start server

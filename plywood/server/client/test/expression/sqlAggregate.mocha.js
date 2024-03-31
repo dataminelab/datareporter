41ed19cc0d30ae9b91c58550df4fc8c4420baf5f
mirkan-samsung-wsl <mirkanbaba1@gmail.com>
2024-03-31 20:38:52 +0300

    Update package.json dependencies and fix test file imports

--- plywood/server/client/test/expression/sqlAggregate.mocha.js
+++ plywood/server/client/test/expression/sqlAggregate.mocha.js
@@ -15,7 +15,7 @@
  */
 
 const { expect } = require('chai');
-const { SqlExpression } = require('druid-query-toolkit');
+const { SqlExpression } = require('@druid-toolkit/query');
 
 let plywood = require('../plywood');
 let { SqlAggregateExpression } = plywood;

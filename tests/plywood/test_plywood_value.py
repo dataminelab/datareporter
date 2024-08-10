ad7478b91ea04c7bdcf36808efcc9af8a1eb8ccb
mirkan-samsung-wsl <mirkanbaba1@gmail.com>
2024-08-11 06:06:01 +0700

    chore: Update PlywoodValue test data and add test for hash_manager

--- tests/plywood/test_plywood_value.py
+++ tests/plywood/test_plywood_value.py
@@ -1,13 +1,14 @@
 import unittest
 
 from redash.plywood.objects.plywood_value import PlywoodValue
+from redash.plywood.hash_manager import cache_or_get
 
 test_data = {
     "attributes": [
         {"name": "main", "type": "DATASET"},
         {"name": "MillisecondsInInterval", "type": "NUMBER"},
         {"name": "added", "type": "NUMBER"},
-        {"name": "SPLIT", "type": "DATASET"}
+        {"name": "SPLIT", "type": "DATASET"},
     ],
     "data": [
         {
@@ -19,7 +20,8 @@ test_data = {
                     {"name": "isNew", "type": "BOOLEAN"},
                     {"name": "main", "type": "DATASET"},
                     {"name": "added", "type": "NUMBER"},
-                    {"name": "SPLIT", "type": "DATASET"}],
+                    {"name": "SPLIT", "type": "DATASET"},
+                ],
                 "data": [
                     {
                         "isNew": False,
@@ -29,11 +31,10 @@ test_data = {
                             "attributes": [
                                 {"name": "isAnonymous", "type": "BOOLEAN"},
                                 {"name": "main", "type": "DATASET"},
-                                {"name": "added", "type": "NUMBER"}
+                                {"name": "added", "type": "NUMBER"},
                             ],
-                            "data": [
-                                {"isAnonymous": True, "added": 37035}
-                            ]}
+                            "data": [{"isAnonymous": True, "added": 37035}],
+                        },
                     },
                     {
                         "isNew": True,
@@ -43,14 +44,15 @@ test_data = {
                             "attributes": [
                                 {"name": "isAnonymous", "type": "BOOLEAN"},
                                 {"name": "main", "type": "DATASET"},
-                                {"name": "added", "type": "NUMBER"}
+                                {"name": "added", "type": "NUMBER"},
                             ],
-                            "data": [
-                                {"isAnonymous": True, "added": 1552}
-                            ]}
-                    }]
-            }}
-    ]
+                            "data": [{"isAnonymous": True, "added": 1552}],
+                        },
+                    },
+                ],
+            },
+        }
+    ],
 }
 
 
@@ -58,6 +60,14 @@ class TestPlywoodValue(unittest.TestCase):
     def test_from_to(self):
         data = PlywoodValue.from_json(test_data)
 
-        self.assertListEqual([a.dict() for a in data.attributes], test_data['attributes'])
+        self.assertListEqual([a.dict() for a in data.attributes], test_data["attributes"])
         self.assertEqual(data.keys, None)
         self.assertEqual(data.dict(), test_data)
+
+    def test_hash_manager(self):
+        queries = [
+            {"id": 1, "name": "query1", "failed_at": "2021-01-01", "message": "error1"},
+            {"id": 2, "name": "query2", "failed_at": "2021-01-02", "message": "error2"},
+        ]
+        cache_result = cache_or_get(hash_string="x", queries=queries, current_org="organisation", model=1, split=1)
+        self.assertAlmostEqual(cache_result, 1.0)

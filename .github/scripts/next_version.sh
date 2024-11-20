196df3c2434412f6e52ed3a52c49a689f5f8f276
jaro <jaroslaw.herod@gmail.com>
2024-11-20 21:38:47 +0100

    chore: fix calculation of next version

--- .github/scripts/next_version.sh
+++ .github/scripts/next_version.sh
@@ -4,14 +4,14 @@ set -e
 GIT_COMMIT=$(git rev-parse --short=8 HEAD)
 
 if [[ "${GITHUB_REF_TYPE-""}" == "tag" ]]; then
-  PREFIX="stable"
-elif [[ "${BRANCH-""}" == "master" ]]; then
+  PREFIX="v"
+elif [[ "${BRANCH-""}" == "main" ]]; then
   PREFIX="stable"
 else
   PREFIX="develop"
 fi
 
-latest=$(git describe --tags --abbrev=0 || echo "0.0.0")
+latest=$(git describe --tags --abbrev=0 2>/dev/null || echo "0.0.0")
 VERSION="${latest#$PREFIX-}"
 VERSION_MAJOR="${VERSION%%.*}"
 VERSION_MINOR_PATCH="${VERSION#*.}"
@@ -33,15 +33,20 @@ elif [[ "${VERSION_MINOR}" != "$(date '+%-m')" ]]; then
   VERSION_MINOR="$(date '+%-m')"
   VERSION_PATCH="0"
 fi
-if [[ "${VERSION_PRE_RELEASE}" != "${GIT_COMMIT}" ]];then
-  VERSION_PATCH=$((VERSION_PATCH+1))
-fi
+
 
 case "$PREFIX" in
  "stable" )
-   NEW_TAG="$PREFIX-${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
+   NEW_TAG="v${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
+   if git rev-list "${NEW_TAG}" > /dev/null  2>&1;then
+     VERSION_PATCH=$((VERSION_PATCH+1))
+   fi
+   NEW_TAG="v${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
    ;;
  "develop" )
+   if [[ "${VERSION_PRE_RELEASE}" != "${GIT_COMMIT}" ]];then
+     VERSION_PATCH=$((VERSION_PATCH+1))
+   fi
    NEW_TAG="$PREFIX-${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}-$GIT_COMMIT"
    ;;
 esac

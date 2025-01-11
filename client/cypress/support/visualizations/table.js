69e72ba327b05213107074e0843726c850505d00
mirkan-ubuntu-wsl <mirkanbaba1@gmail.com>
2025-01-12 00:25:02 +0300

    feat: update Cypress Dockerfile and tests for improved compatibility and functionality

--- client/cypress/support/visualizations/table.js
+++ client/cypress/support/visualizations/table.js
@@ -1,12 +1,14 @@
 export function expectTableToHaveLength(length) {
   cy.getByTestId("TableVisualization")
     .find("tbody tr")
+    .then($rows => ($rows[0].classList.contains("ant-table-measure-row") ? $rows.slice(1) : $rows))
     .should("have.length", length);
 }
 
 export function expectFirstColumnToHaveMembers(values) {
   cy.getByTestId("TableVisualization")
     .find("tbody tr td:first-child")
+    .then($cells => $cells.filter("td.ant-table-cell.display-as-string"))
     .then($cell => Cypress.$.map($cell, item => Cypress.$(item).text()))
     .then(firstColumnCells => expect(firstColumnCells).to.have.members(values));
 }

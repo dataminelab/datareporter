e9acc8832933ff8679770d366a6bcc919e60949c
mirkan-ubuntu-wsl <mirkanbaba1@gmail.com>
2025-05-18 21:12:53 +0300

    fix: comment out unused moduleNameMapper entry for @redash/viz

--- client/jest.config.js
+++ client/jest.config.js
@@ -31,7 +31,7 @@ module.exports = {
         "/node_modules/(?!(viz-lib)/)"
     ],
     moduleNameMapper: {
-        "^@redash/viz(.*)$": "<rootDir>/../viz-lib$1",
+        //"^@redash/viz(.*)$": "<rootDir>/../viz-lib$1",
         "^react$": "<rootDir>/../client/node_modules/react",
         "^@/(.*)": "<rootDir>/app/$1",
         "\\.(css|less)$": "identity-obj-proxy"

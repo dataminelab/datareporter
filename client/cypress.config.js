dc26d97e06ea053f511e881af5a5cf17c64eeaf5
mirkan-ubuntu-wsl <mirkanbaba1@gmail.com>
2025-05-20 21:13:56 +0300

    fix: update Cypress configuration to include browser launch options for improved performance

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -19,6 +19,16 @@ module.exports = defineConfig({
       coverage: false,
     },
     experimentalSessionAndOrigin: true,
+    setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome' || browser.name === 'chromium') {
+          launchOptions.args.push('--disable-gpu')
+          launchOptions.args.push('--no-sandbox')
+          launchOptions.args.push('--disable-dev-shm-usage')
+        }
+        return launchOptions
+      })
+    },
   },
 
   component: {

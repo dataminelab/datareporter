6cce57691f04bb8e5231fc49a7a786f628521817
mirkan-ubuntu-wsl <mirkanbaba1@gmail.com>
2025-05-23 15:22:02 +0300

    omit docker compose comand on run-ci

--- client/cypress/cypress.js
+++ client/cypress/cypress.js
@@ -78,7 +78,7 @@ function runCypressCI() {
   }
 
   execSync(
-    "COMMIT_INFO_MESSAGE=$(git show -s --format=%s) docker compose up -d ./node_modules/.bin/percy exec -t 300 -- ./node_modules/.bin/cypress run --record",
+    "COMMIT_INFO_MESSAGE=$(git show -s --format=%s) ./node_modules/.bin/percy exec -t 300 -- ./node_modules/.bin/cypress run --record",
     { stdio: "inherit" }
   );
 }

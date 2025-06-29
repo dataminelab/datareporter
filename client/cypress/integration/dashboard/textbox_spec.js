ba08914c46bd96f72b0bc4aba2a9d217a2722df6
mirkan-ubuntu-wsl <mirkanbaba1@gmail.com>
2025-06-29 14:36:28 +0300

    fix: enhance Cypress test execution and improve height assertions in dashboard tests

--- client/cypress/integration/dashboard/textbox_spec.js
+++ client/cypress/integration/dashboard/textbox_spec.js
@@ -142,7 +142,7 @@ describe("Textbox", () => {
       })
       .should($el => {
         const { top, left } = $el.offset();
-        expect(top).to.be.oneOf([232, 232.015625, 162, 222.015625]);
+        expect(top).to.be.oneOf([232, 232.015625, 162, 222.015625, 222.984375]);
         expect(left).to.be.oneOf([215, 282]);
         expect($el.width()).to.be.oneOf([585, 545]);
         expect($el.height()).to.eq(185);

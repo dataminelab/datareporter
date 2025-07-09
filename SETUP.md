38b3f8a54dad4121a545771d63a777cfbe7ad590
mirkan-ubuntu-wsl <mirkanbaba1@gmail.com>
2025-07-09 05:36:28 +0300

    feat: add Ollama service to Docker Compose and integrate Deepseek model in Dashboard

--- SETUP.md
+++ SETUP.md
@@ -47,7 +47,8 @@ nvm use v18.20 > /dev/null
 docker compose up --build # or make up to start required services like postgres app server
 docker compose run --rm server create_db # start server and run. exec /app/manage.py database create_tables. 
 # Database Update process
-docker-compose run server manage db stamp head # If you get an error saying "target database is not up to date" you can run this command
+docker compose exec ollama ollama pull deepseek-r1:7b # Pull deepseek-r1:7b model from ollama
+docker compose run server manage db stamp head # If you get an error saying "target database is not up to date" you can run this command
 docker compose run server manage db migrate # Any change to back-end models requires to create a migration
 docker compose run --rm server manage db upgrade # Upgrade database with recent migration
 ```
--- client/app/pages/dashboards/DashboardPage.jsx
+++ client/app/pages/dashboards/DashboardPage.jsx
@@ -36,6 +36,38 @@ import { RelativeTimeFilterClause, FixedTimeFilterClause } from "@/components/Tu
 import { TimeShift } from "@/components/TurniloComponent/common/models/time-shift/time-shift";
 import { DateRange } from "@/components/TurniloComponent/common/models/date-range/date-range";
 
+
+async function run() {
+    let prompt = document.querySelector("#question").value
+
+    const response = await fetch("http://localhost:11434/api/generate", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            model: "deepseek-r1:7b",
+            prompt: prompt,
+            stream: true
+        })
+    })
+
+    const reader = response.body.getReader()
+    const decoder = new TextDecoder()
+
+    let compiledResponse = ""
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const chunk = decoder.decode(value, { stream: true });
+        let chunkJson = JSON.parse(chunk)
+        compiledResponse += chunkJson.response
+        compiledResponse = compiledResponse.replace("<think>", `<div id="think">`)
+        compiledResponse = compiledResponse.replace("</think>", `</div>`)
+        document.querySelector("#answer").innerHTML = compiledResponse;
+    }
+}
+
 class DashboardSettings extends React.Component {
   static propTypes = {
     dashboardOptions: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
@@ -428,6 +460,17 @@ class DashboardComponent extends React.Component {
             setFilterParams={this.setFilterParams}
             getEssence={this.getEssence}
           />
+        </div>
+        {/* UNSAFE: Directly injecting HTML for demonstration purposes */}
+        <div>
+          <h1>🤖 Local Deepseek</h1>
+
+          <textarea name="" id="question"></textarea>
+          <button onClick={() => run()}>Ask!</button>
+
+          <div id="answer">
+
+          </div>
         </div>
       </div>
     );

bfffc84c4b01e3f273bc79cdec3ad15adc1d33d6
mirkan-ubuntu-wsl <mirkanbaba1@gmail.com>
2025-07-20 21:59:15 +0300

    refactor: improve prompt generation for dashboard queries and add validation for prompt value

--- client/app/pages/dashboards/components/DashboardHeader.jsx
+++ client/app/pages/dashboards/components/DashboardHeader.jsx
@@ -182,14 +182,14 @@ DashboardMoreOptionsButton.propTypes = {
 async function getPromptAnswer(promptValue) {
     const url = window.location.pathname.split('/').pop()?.split('?')[0] || '';
     const datasets = window.loadedDatasetsByUrl[url];
-    let prompt = "You are a dashboard analyst. You will be given a question and you will answer it in the form of a dashboard query. ";
-    prompt += "Datasets are as below, you shall use them to answer the question. if there are some giant values such as `MillisecondsInInterval` please ignore them and do not use them in your answer. ";
-    datasets.forEach((d, i) => {
-        prompt += `\n\n Dataset ${i + 1}:\n`;
-        prompt += JSON.stringify(d);
-    });
+    let prompt = "You are a dashboard analyst. There are datasets inside a a Dashboard aka widget. You will be given a question and you will answer it in the form of human language. ";
     prompt += "\n\n";
-    prompt += "Answer the question in the form of a dashboard query. Question is as below:\n";
+    prompt += "Datasets are as below: ";
+    prompt += "\n";
+    datasets.forEach((d, i) => {
+      prompt += `Dataset[${i + 1}]: ${JSON.stringify(d)}\n`;
+    });
+    prompt += "\n---\n\n";
     prompt += promptValue;
 
     const response = await fetch("http://localhost:11434/api/generate", {
@@ -250,6 +250,7 @@ function DashboardControl({ dashboardConfiguration, headerExtra }) {
   const [sendingPrompt, setSendingPrompt] = React.useState(false);
 
   const handlePromptSend = async () => {
+    if (!promptValue) return;
     setSendingPrompt(true);
     const answer = await getPromptAnswer(promptValue);
     setPromptAnswerValue(answer);

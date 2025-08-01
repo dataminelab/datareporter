0b681c19f35a9f8e8c8df846487db1b56b97fda3
Restyled.io <commits@restyled.io>
2025-08-01 17:00:02 +0000

    Restyled by prettier

--- client/cypress/support/redash-api/index.js
+++ client/cypress/support/redash-api/index.js
@@ -1,6 +1,8 @@
 const { extend, get, merge, find } = Cypress._;
 
-const post = options => //
+const post = (
+  options, //
+) =>
   cy
     .getCookie("csrf_token")
     .then(csrf => cy.request({ ...options, method: "POST", headers: { "X-CSRF-TOKEN": csrf.value } }));

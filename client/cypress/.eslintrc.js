4b60d9ebcd5cbfd7eb71a6a5e149f05e43deebf3
mirkan-ubuntu-wsl <mirkanbaba1@gmail.com>
2025-08-01 20:10:27 +0300

    refactor: remove restyled job dependency and clean up linting configuration

--- client/cypress/.eslintrc.js
+++ client/cypress/.eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     "@typescript-eslint/no-empty-function": "off",
     "no-unused-vars": "warn",
     "@typescript-eslint/no-unused-vars": "warn",
+    "no-redeclare": "warn",
   },
 };

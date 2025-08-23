a37382674aa3381896bf842db2e2c17e000d0ebc
mirkan-wsl <mirkanbaba1@gmail.com>
2025-08-23 11:58:30 +0300

    Update environment variables and modify Cypress command for Percy finalization

--- client/cypress/cypress.js
+++ client/cypress/cypress.js
@@ -89,7 +89,7 @@ function runCypressCI() {
     { stdio: "inherit" },
   );
   execSync(
-    "docker compose run --rm cypress ./node_modules/.bin/percy build:finalize",
+    "docker compose run --rm cypress ./node_modules/.bin/percy finalize",
     { stdio: "inherit" },
   );
 }

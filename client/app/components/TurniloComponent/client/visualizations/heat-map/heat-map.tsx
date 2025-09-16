2bc0a7e46a6e70411976d57fba59c8231db47449
mirkan-wsl <mirkanbaba1@gmail.com>
2025-09-16 13:56:24 +0200

    fix: correct comment formatting in heat-map.tsx

--- client/app/components/TurniloComponent/client/visualizations/heat-map/heat-map.tsx
+++ client/app/components/TurniloComponent/client/visualizations/heat-map/heat-map.tsx
@@ -14,8 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-// For some reason tsc compiler does not see this file.
+// For some reason tsc compiler does not see this
+import "../../../types/index.d.ts";
 import memoizeOne from "memoize-one";
 import { Dataset } from "plywood";
 import * as React from "react";

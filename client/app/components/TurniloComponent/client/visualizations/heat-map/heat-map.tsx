1f43c0674a889a8f076626942684755ac853628e
mirkan-wsl <mirkanbaba1@gmail.com>
2025-09-19 15:46:27 +0200

    fix: remove unused import statement for type definitions in heat-map.tsx

--- client/app/components/TurniloComponent/client/visualizations/heat-map/heat-map.tsx
+++ client/app/components/TurniloComponent/client/visualizations/heat-map/heat-map.tsx
@@ -14,8 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// For some reason tsc compiler does not see this
-import "../../types/index.js";
 import memoizeOne from "memoize-one";
 import { Dataset } from "plywood";
 import * as React from "react";

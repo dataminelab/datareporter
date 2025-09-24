b26894c61c4522e0d521b09e6502f62f67336f64
mirkan-wsl <mirkanbaba1@gmail.com>
2025-09-24 17:34:26 +0200

    fix: update scroller reference in Table component to use createRef

--- client/app/components/TurniloComponent/client/visualizations/table/table.tsx
+++ client/app/components/TurniloComponent/client/visualizations/table/table.tsx
@@ -76,6 +76,7 @@ export interface TableState extends BaseVisualizationState {
 export class Table extends BaseVisualization<TableState> {
   protected className = TABLE_MANIFEST.name;
   protected innerTableRef = React.createRef<HTMLDivElement>();
+  private scrollerRef = React.createRef<Scroller>();
 
   getDefaultState(): TableState {
     return {
@@ -310,7 +311,7 @@ export class Table extends BaseVisualization<TableState> {
           value={segmentWidth}
         />
         <Scroller
-          ref="scroller"
+          ref={this.scrollerRef}
           layout={scrollerLayout}
           topGutter={
             <MeasuresHeader

b8a3e2064f7542f5243759dbb9ab963f9eec3da7
mirkan-wsl <mirkanbaba1@gmail.com>
2025-11-06 00:25:18 +0300

    feat: add Worker Debugger configuration and refactor restart scripts for improved server management

--- bin/restart_server.sh
+++ bin/restart_server.sh
@@ -10,14 +10,13 @@ if [ "$state" = "yes" ] || [ "$state" = "first" ]; then
     docker stop datareporter-server-1
     docker rm datareporter-server-1
     docker rmi datareporter-server
-    docker compose -f docker-compose.yml up -d
 else
     echo "Restarting server without rebuilding the image"
     docker stop datareporter-server-1
     docker rm datareporter-server-1
     export skip_frontend_build="do not build front-end"
-    docker compose -f docker-compose.yml up -d
 fi
 
+docker compose -f docker-compose.yml up -d
 docker compose -f docker-compose.yml run --rm server create_db
 docker compose -f docker-compose.yml stop server && docker compose -f docker-compose.yml run --rm --service-ports server debug && docker compose -f docker-compose.yml start server
--- bin/restart_worker_server.sh
+++ bin/restart_worker_server.sh
@@ -1,11 +1,23 @@
-echo "Restarting worker server without rebuilding the image"
-docker stop datareporter-worker-server-1 && \
-docker rm datareporter-worker-server-1 && \
-docker rmi datareporter-worker-server-1
-export skip_dev_deps="do not update dev dependencies"
-export skip_ds_deps="do not update ds dependencies"
-export skip_frontend_build="do not build front-end"
-docker-compose up -d
-# only up worker server
-docker start datareporter-worker-server-1
-docker-compose stop worker-server && docker-compose run --rm --service-ports worker-server debug && docker-compose start worker-server
+#!/bin/bash
+if [ "$1" ]; then
+    state="$1"
+else
+    state="no"
+fi
+
+if [ "$state" = "yes" ] || [ "$state" = "first" ]; then
+    echo "Stopping and removing the worker server container"
+    docker stop datareporter-worker-server-1
+    docker rm datareporter-worker-server-1
+    docker rmi datareporter-worker-server
+else
+    echo "Restarting worker server without rebuilding the image"
+    docker stop datareporter-worker-server-1
+    docker rm datareporter-worker-server-1
+    export skip_dev_deps="do not update dev dependencies"
+    export skip_ds_deps="do not update ds dependencies"
+    export skip_frontend_build="do not build front-end"
+fi
+
+docker compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml stop worker-server && docker compose -f docker-compose.yml run --rm --service-ports worker-server debug && docker compose -f docker-compose.yml start worker-server

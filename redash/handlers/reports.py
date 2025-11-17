4923573fd5727841dfd5c953d061db45386a2f94
mirkan-wsl <mirkanbaba1@gmail.com>
2025-11-17 14:40:14 +0300

    fix: improve authentication check in ReportGenerateResource to handle unauthenticated users more effectively

--- redash/handlers/reports.py
+++ redash/handlers/reports.py
@@ -154,7 +154,7 @@ class ReportApiKeyAccess(BaseResource):
 # /api/reports/generate/<int:model_id>
 class ReportGenerateResource(BaseResource):
     def post(self, model_id):
-        if not self.current_user.is_authenticated and not isinstance(self.current_user, models.ApiUser):
+        if not self.current_user or (not self.current_user.is_authenticated and not isinstance(self.current_user, models.ApiUser)):
             abort(405)
 
         req = request.get_json(True)

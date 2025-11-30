0d09d75a8c46200ac595eccae75fcfc9b3c3c9a4
mirkan-wsl <mirkanbaba1@gmail.com>
2025-11-30 21:34:49 +0300

    fix: handle undefined tickFormat in prepareXAxis and prepareYAxis functions

--- viz-lib/src/visualizations/chart/plotly/prepareLayout.js
+++ viz-lib/src/visualizations/chart/plotly/prepareLayout.js
@@ -21,7 +21,7 @@ function prepareXAxis(axisOptions, additionalOptions) {
     title: getAxisTitle(axisOptions),
     type: getAxisScaleType(axisOptions),
     automargin: true,
-    tickformat: axisOptions.tickFormat,
+    tickformat: axisOptions.tickFormat || null,
   };
 
   if (additionalOptions.sortX && axis.type === "category") {
@@ -46,7 +46,7 @@ function prepareYAxis(axisOptions) {
     automargin: true,
     autorange: true,
     range: null,
-    tickformat: axisOptions.tickFormat,
+    tickformat: axisOptions.tickFormat || null,
   };
 }
 

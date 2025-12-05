d7ac26291b55f825368ecb7186c44a7ef478cc1c
mirkan-wsl <mirkanbaba1@gmail.com>
2025-12-05 23:45:50 +0300

    fix: update conditions for adding artifact links and improve debug script

--- bin/restart_plywood.sh
+++ bin/restart_plywood.sh
@@ -1,8 +1,12 @@
+# this script restarts plywood with latest changes
+# runs it on local on debug mode
 cd client
 npm run build:plywood
 npm run build:plywood-server
 cd ..
 docker-compose stop plywood
 docker-compose rm -f plywood
-docker-compose up -d plywood
-docker-compose logs -f plywood
+docker-compose up -d plywood -d
+docker-compose stop plywood
+cd plywood
+npm run dev:debug

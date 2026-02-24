7c85e32870a07953e8fdbdcdf0f0671ad5f821a8
mirkan-wsl <mirkanbaba1@gmail.com>
2026-02-24 15:38:27 +0300

    feat: enhance report serialization with user details and data source ID

--- redash/settings/__init__.py
+++ redash/settings/__init__.py
@@ -264,6 +264,7 @@ MAIL_DEFAULT_SENDER = os.environ.get("REDASH_MAIL_DEFAULT_SENDER", None)
 MAIL_MAX_EMAILS = os.environ.get("REDASH_MAIL_MAX_EMAILS", None)
 MAIL_ASCII_ATTACHMENTS = parse_boolean(os.environ.get("REDASH_MAIL_ASCII_ATTACHMENTS", "false"))
 
+
 def email_server_is_configured():
     return MAIL_DEFAULT_SENDER is not None
 

14010836d38588d42c0051c0b2170b7896502994
Radek <radek@dataminelab.com>
2026-02-26 00:17:23 +0200

    Fixing frontend unit tests

--- client/app/components/TurniloComponent/common/utils/canonical-length/split-canonical-length.ts
+++ client/app/components/TurniloComponent/common/utils/canonical-length/split-canonical-length.ts
@@ -22,11 +22,7 @@ export default function splitCanonicalLength(split: Split, dataCube: DataCube): 
   const { reference, bucket } = split;
   if (!bucket) return null;
   if (reference !== dataCube.timeAttribute.name) return null;
-  const durationBucket =
-    bucket instanceof Duration
-      ? bucket
-      : // @ts-ignore - Handle both ISO string (P1W) and Duration-like plain objects
-        Duration.fromJS(bucket);
+  if (!(bucket instanceof Duration)) return null;
 
-  return durationBucket.getCanonicalLength();
+  return bucket.getCanonicalLength();
 }

6d040a0915563dd00157a856ebbf18acb549f918
Radek <radek@dataminelab.com>
2026-02-26 01:43:23 +0200

    Prettify

--- client/app/components/QuerySelector.jsx
+++ client/app/components/QuerySelector.jsx
@@ -20,15 +20,16 @@ function search(term) {
   // get recent
   if (!term) {
     const recentQueries = Query.recent().then(results =>
-      results.filter(item => !item.is_draft).map(query => ({ ...query, type: "query" })),
+      results
+        .filter(item => !item.is_draft)
+        .map(query => ({ ...query, type: "query" })),
     );
     const recentReports = Report.recent().then(results =>
       results.map(report => ({ ...report, type: "report" })),
     );
-    return Promise.all([recentReports, recentQueries]).then(([reports, queries]) => [
-      ...reports,
-      ...queries,
-    ]);
+    return Promise.all([recentReports, recentQueries]).then(
+      ([reports, queries]) => [...reports, ...queries],
+    );
   }
 
   // search by query and report

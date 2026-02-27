6f3d0a79fad209856ecc9c366dfd25e64fcf83a3
mirkan-wsl <mirkanbaba1@gmail.com>
2026-02-27 18:41:10 +0300

    Add get_schema method to Mysql class and improve table schema retrieval logic

--- redash/query_runner/mysql.py
+++ redash/query_runner/mysql.py
@@ -239,6 +239,33 @@ class Mysql(BaseSQLQueryRunner):
 
         return error
 
+    def get_schema(self, get_stats=False):
+        if self.configuration.get("glue", False):
+            catalog_ids = [id.strip() for id in self.configuration.get("catalog_ids", "").split(",")]
+            return sum([self.__get_schema_from_glue(catalog_id) for catalog_id in catalog_ids], [])
+
+        schema = {}
+        query = """
+        SELECT LOWER(table_schema) AS table_schema,
+            LOWER(table_name) AS table_name,
+            LOWER(column_name) AS column_name,
+            LOWER(data_type) AS data_type
+        FROM information_schema.columns
+        WHERE table_schema NOT IN ('information_schema')
+        """
+
+        results, error = self.run_query(query, None)
+        if error is not None:
+            self._handle_run_query_error(error)
+
+        for row in results["rows"]:
+            table_name = "{0}.{1}".format(row["table_schema"], row["table_name"])
+            if table_name not in schema:
+                schema[table_name] = {"name": table_name, "columns": []}
+            schema[table_name]["columns"].append({"name": row["column_name"], "type": row["data_type"]})
+
+        return list(schema.values())
+
 
 class RDSMySQL(Mysql):
     @classmethod
--- redash/services/model_config_generator.py
+++ redash/services/model_config_generator.py
@@ -150,7 +150,7 @@ class ModelConfigGenerator:
     @staticmethod
     def _build(model: Model, refresh):
         schemas = model.data_source.get_schema(refresh=refresh)
-        table_schema = next((schema for schema in schemas if schema["name"] == model.table), None)
+        table_schema = next((schema for schema in schemas if model.table in schema["name"]), None)
         if table_schema is None:
             raise ValueError("Data source {} doesn't contain {} table".format(model.data_source, model.table))
 
